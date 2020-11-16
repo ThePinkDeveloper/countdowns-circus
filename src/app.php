ad10b6c86444173dc9aff72e7fde4b4f0fe29be1
Miguel Rosa <nmlbstudio@gmail.com>
2020-11-16 23:38:37 +0100

    [MAIN] First commit

---
+++ src/app.php
@@ -0,0 +1,7 @@
+<?php
+
+include 'container.php';
+include 'middleware.php';
+include 'routes/web.php';
+
+$app -> run();
---
+++ src/container.php
@@ -0,0 +1,17 @@
+<?php
+
+use DI\Container;
+use Carbon\Carbon;
+use Slim\Factory\AppFactory;
+
+$container = new Container();
+
+AppFactory::setContainer($container);
+
+$container -> set('carbon', function() {
+    return new Carbon();
+});
+
+
+// last line always
+$app = AppFactory::create();
